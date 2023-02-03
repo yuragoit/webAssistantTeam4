530ecc4aa6d6fc263b7794a37d278114a97b19a4
yuragoit <101989870+yuragoit@users.noreply.github.com>
2023-02-03 12:45:15 +0200

    Update urls (add root files, auth)
    Update urls (add root files, auth)

--- project/config/urls.py
+++ project/config/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r"^files/", include("apps.file_storage.urls", namespace="file_storage")),
+    re_path(r"^auth/", include("apps.user_auth.urls", namespace="user_auth")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
