862bdbccd94059e899102f9844270fbb9f1d0d9a
taras <spasibovtaras@gmail.com>
2023-02-10 21:51:39 +0200

    add searching & phones validations

--- apps/contacts/models.py
+++ apps/contacts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 # Create your models here.
@@ -9,7 +10,7 @@ class AddressBook(models.Model):
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=20)
+    phone_number = PhoneNumberField(null=False, blank=False, unique=True)
     email = models.EmailField(max_length=100, blank=True, null=True)
     address = models.CharField(max_length=256, blank=True, null=True)
     birthday = models.DateField(blank=True, null=True)
