afe5658204a7a34e41eb4098d1c72d80268a3a14
Petrik Dmutro <108899164+PetrikDima@users.noreply.github.com>
2023-02-12 02:20:09 +0300

    make UI
    make UI for file storage app

--- apps/storage/models.py
+++ apps/storage/models.py
@@ -6,8 +6,8 @@ from .сonstans import MAX_CHAR_LENGTH
 
 # Create your models here.
 class File(models.Model):
-    file = models.FileField()
     name = models.CharField(max_length=MAX_CHAR_LENGTH, null=True)
     type = models.CharField(max_length=MAX_CHAR_LENGTH, null=True)
     upload_at = models.DateTimeField(auto_now_add=True)
+    size = models.FloatField(default=0.0)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
--- apps/storage/views.py
+++ apps/storage/views.py
@@ -20,14 +20,61 @@ def get_folder_name(filename):
 
 @login_required(login_url="/login/")
 def upload(request):
-    if request.method == 'POST' and request.FILES['upload_file']:
+    if request.method == 'POST' and request.FILES.get('upload_file'):
         file = request.FILES['upload_file']
+
         filename = file.name
         if os.path.exists(Path(f'{UPLOADS_DIR}/{get_folder_name(filename)}/{filename}')):
             os.remove(Path(f'{UPLOADS_DIR}/{get_folder_name(filename)}/{filename}'))
+
         fs = FileSystemStorage(location=Path(f'{UPLOADS_DIR}/{get_folder_name(filename)}'))
         fs.save(name=filename, content=file)
-        # model_file = File(file=file, name=filename, type=get_folder_name(filename), owner=request.user)
-        # model_file.save()
+
+        if not File.objects.filter(name=filename, owner=request.user).first():
+            model_file = File(name=filename, type=get_folder_name(filename), owner=request.user, size=file.size)
+            model_file.save()
+
         return redirect('home/app_file_storage.html')
     return render(request, 'home/app_file_storage.html')
+
+
+def render_storage_pages(request, file_type):
+    files = File.objects.filter(type=file_type, owner=request.user).all()
+    title = file_type
+    return render(request, 'storage/files.html', context={'files': files,
+                                                          'title': title})
+
+
+@login_required(login_url="/login/")
+def archives(request):
+    return render_storage_pages(request, 'Archives')
+
+
+@login_required(login_url="/login/")
+def images(request):
+    return render_storage_pages(request, 'Images')
+
+
+@login_required(login_url="/login/")
+def video(request):
+    return render_storage_pages(request, 'Video')
+
+
+@login_required(login_url="/login/")
+def documents(request):
+    return render_storage_pages(request, 'Documents')
+
+
+@login_required(login_url="/login/")
+def programs(request):
+    return render_storage_pages(request, 'Programs')
+
+
+@login_required(login_url="/login/")
+def audio(request):
+    return render_storage_pages(request, 'Audio')
+
+
+@login_required(login_url="/login/")
+def others(request):
+    return render_storage_pages(request, 'Others')
