999b9fd250d5379a4e88c075320b255ad8ac7d11
DmytroLievoshko <levka296@gmail.com>
2023-02-12 22:11:32 +0200

    add notes_list and note_create

--- apps/noteapp/models.py
+++ apps/noteapp/models.py
@@ -4,14 +4,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Tag(models.Model):
-    name = models.CharField(max_length=25, unique=True, null=False)
+    name = models.CharField(max_length=25, null=False)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.name
 
 
 class Note(models.Model):
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, default=1)
     name = models.CharField(max_length=50, null=False)
     description = models.CharField(max_length=150, null=False)
     done = models.BooleanField(default=False)
--- apps/noteapp/urls.py
+++ apps/noteapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from . import views
-# from .views import app_notes
+from .views import NotesListView, NotesDeleteView, note
+
+app_name = "notes"
 
 urlpatterns = [
-    # The home page
-    path("", views.app_notes, name="app_notes")
+    path("", NotesListView.as_view(), name="notes_list"),
+    path("create/", note, name="note_create"),
+    path("detail/<int:pk>/", NotesListView.as_view(), name="note_detail"),
+    path("update/<int:pk>/", NotesListView.as_view(), name="note_update"),
+    path("delete/<int:pk>/", NotesDeleteView.as_view(), name="note_delete"),
 ]
